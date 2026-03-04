7ed6b55812b30ccb76ae9d43135376d7b88c550d
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-05 06:20:02 +0700

    feat: add not found and dashboard pages

---
+++ src/components/Dashboard.js
@@ -0,0 +1,36 @@
+import { el } from "../utils.js";
+import { initHeader } from "./Header.js";
+
+export function initDashboard() {
+  const header = initHeader();
+
+  const dashboard = el("div", {
+    className: "flex flex-col flex-1 items-center h-full w-full",
+  });
+
+  const page = el("div", {
+    className: "flex flex-1 items-start justify-center py-2 w-full",
+  });
+
+  const pageContent = el("div", {
+    className:
+      "flex flex-1 flex-col items-start justify-start mx-3 max-w-225 gap-2",
+  });
+
+  const pageTitle = el("h1", {
+    text: "Dashboard",
+    className: "text-4xl font-bold text-zinc-950 dark:text-white",
+  });
+
+  const description = el("p", {
+    text: "Here will be the dashboard content.",
+
+    className: "text-sm text-gray-400",
+  });
+
+  pageContent.append(pageTitle, description);
+  page.append(pageContent);
+  dashboard.append(header, page);
+
+  return dashboard;
+}
